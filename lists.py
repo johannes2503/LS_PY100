ccd83dccc1b03026eaa56dfd42a3e8e16fb63c2f
Johannes David Hreinsson <46741753+johannes2503@users.noreply.github.com>
2025-04-14 21:22:57 +0000

    Working on lists exercise

--- lists.py
+++ lists.py
@@ -72,10 +72,99 @@
 # Filter
 
 
-scores = [96, 47, 113, 89, 100, 102]
+# Version 1 - Solution
+
+# scores = [96, 47, 113, 89, 100, 102]
+
+# count = 0
+
+# for score in scores:
+#     if score >= 100:
+#         count += 1
+
+# print(count)
+
+
+# Vocabulary
+
+# Version 1
+
+# vocabulary = [
+#     ['happy', 'cheerful', 'merry', 'glad'],
+#     ['tired', 'sleepy', 'fatigued', 'drained'],
+#     ['excited', 'eager', 'enthused', 'animated'],
+# ]
+
+# for vocab in vocabulary:
+#     for vocab2 in vocab:
+#         print(vocab2)
+
+
+# # happy
+# # cheerful
+# # merry
+# # glad
+# # tired
+# # sleepy
+# # etc...
+
+
+# Equality
+
+# Version 1
+
+# list1 = [2, 6, 4]
+# list2 = [2, 6, 4]
+
+# print(list1 == list2)
 
 
 
+# Type
+
+# Version 1
+
+# some_value1 = [0, 1, 0, 0, 1]
+# some_value2 = 'I leave you my Kingdom, take good care of it.'
+
+# print(type(some_value1) is list)
+# print(type(some_value2) is list)
+
+
+# Version 2 - Solution
+
+# some_value1 = [0, 1, 0, 0, 1]
+# some_value2 = 'I leave you my Kingdom, take good care of it.'
+
+# isinstance(some_value1, list)  # True
+# isinstance(some_value2, list)  # False
 
 
 
+# Travel 
+
+# Version 1
+
+# destinations = ['Prague', 'London', 'Sydney', 'Belfast',
+#                 'Rome', 'Aruba', 'Paris', 'Bora Bora',
+#                 'Barcelona', 'Rio de Janeiro', 'Marrakesh',
+#                 'New York City']
+
+# def contains(city, destinations):
+#     if 'Nashville' in city:
+#         print(True)
+#     else:
+#         print(False)
+    
+# Version 2 - Solution
+
+# def contains(element, destination):
+#     for item in destination:
+#         if item == element:
+#             return True
+
+#     return False
+    
+
+# contains('Barcelona', destinations)  # True
+# contains('Nashville', destinations)  # False
